7cfcbb7c04df274e97c61e8466c8c882bea78863
Tomoya Ohnishi <t.ohnishi@tumh.ac.jp>
2026-06-01 14:38:35 +0900

    Update start_py_env.bat

--- start_py_env.bat
+++ start_py_env.bat
@@ -1,12 +1,9 @@
-@echo off
-
+Set-Content -Path .\start_py_env.bat -Encoding ASCII -Value '@echo off
 cd /d "%~dp0"
 
 if not exist "env\Scripts\python.exe" (
-    echo 仮想環境を作成しています...
     python -m venv env
-) else (
-    echo 仮想環境は既に存在します。
 )
 
 pause
+'
