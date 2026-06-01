df5241d502c2f92c6ae0908e4e59e5fd12e75e43
Tomoya Ohnishi <t.ohnishi@tumh.ac.jp>
2026-06-01 14:44:31 +0900

    Update start_py_env.bat

--- start_py_env.bat
+++ start_py_env.bat
@@ -1,23 +1,8 @@
 @echo off
-chcp 65001 > nul
-setlocal
+cd /d "%~dp0"
 
-set "BASE_DIR=%~dp0"
-set "VENV_DIR=%BASE_DIR%env"
-set "PYTHON_CMD=python"
-
-where py > nul 2>&1
-if %errorlevel%==0 (
-    set "PYTHON_CMD=py"
+if not exist "env\Scripts\python.exe" (
+    python -m venv env
 )
 
-if not exist "%VENV_DIR%\Scripts\python.exe" (
-    echo 仮想環境を作成しています...
-    %PYTHON_CMD% -m venv "%VENV_DIR%"
-)
-
-echo 仮想環境の準備が完了しました。
 pause
-
-endlocal
-exit
