89d48b752c6622ac4f69a52e88051315339c0c42
Tomoya Ohnishi <t.ohnishi@tumh.ac.jp>
2026-06-01 14:55:42 +0900

    Update start_py_env.bat

--- start_py_env.bat
+++ start_py_env.bat
@@ -1,8 +1,15 @@
 @echo off
-cd /d "%~dp0"
 
 if not exist "env\Scripts\python.exe" (
     python -m venv env
 )
+
+call env\Scripts\activate.bat
+
+if not exist "main.py" (
+    type nul > main.py
+)
+
+python main.py
 
 pause
