6279d5d68cf3aa088ed988b81354ea4c88b2c92a
Tomoya Ohnishi <t.ohnishi@tumh.ac.jp>
2026-06-01 15:04:20 +0900

    Update start_py_env.bat

--- start_py_env.bat
+++ start_py_env.bat
@@ -7,7 +7,11 @@ if not exist "env\Scripts\python.exe" (
 call env\Scripts\activate.bat
 
 if not exist "main.py" (
-    type nul > main.py
+    (
+        echo # Created: %date% %time%
+        echo.
+        echo print("Hello Python")
+    ) > main.py
 )
 
 python main.py
